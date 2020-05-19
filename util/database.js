22675192c97f401159b91234fd955c65e080e17d
Arun Mohan <arunmh7@gmail.com>
2020-05-19 11:54:24 +0530

    added mongoDb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+
+
+const mongoConnect = (cb) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log('connected');
+      cb(client);
+    })
+    .catch(err => console.log(err))
+}
+
+module.exports = mongoConnect;
